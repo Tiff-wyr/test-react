e046c83c67b9632d6f390493d4c080ef5f99a2db
gengfeiyang <feiyanggeng@yufuid.com>
2020-07-14 17:12:13 +0800

    代码整理

--- src/App.js
+++ src/App.js
@@ -1,161 +1,224 @@
-import React, {Component} from 'react';
-import './App.css';
-import ListItem from './page/ListItem'
-import Count from './page/Count';
-import List from "./page/List"
+import React, { Component } from "react";
+import "./App.css";
+import ListItem from "./page/ListItem";
+import Count from "./page/Count";
+import List from "./page/List";
 
-import store from "./store"
-console.log(store.getState())
+import store from "./store";
+console.log(store.getState());
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    console.log("初始化app")
+    super(props);
+    console.log("初始化app");
     this.state = {
       arr: [
         {
-          id: '1',
+          id: "1",
           checked: true,
           text: "语文",
-          del: false
+          del: false,
         },
         {
-          id: '2',
+          id: "2",
           checked: false,
           text: "数学",
-          del: false
+          del: false,
         },
         {
-          id: '3',
+          id: "3",
           checked: false,
           text: "英语",
-          del: false
+          del: false,
         },
         {
-          id: '4',
+          id: "4",
           checked: false,
           text: "文综",
-          del: false
-        }
+          del: false,
+        },
       ],
       showArr: [],
-      text: '',
-      status: 'all'
-    }
+      text: "",
+      status: "all",
+    };
   }
   componentDidMount() {
-    this.switchShowArr(this.state.status)
+    this.switchShowArr(this.state.status);
   }
   inputHandle(e) {
     this.setState({
-      text: e.target.value
-    })
+      text: e.target.value,
+    });
   }
   keyDownHandle(e) {
     if (e.keyCode === 13) {
-      this.addList(e)
+      this.addList(e);
     }
   }
-  addList (event) {
-    let text = event.target.value
-    if (!text) return
+  addList(event) {
+    let text = event.target.value;
+    if (!text) return;
     let obj = {
       checked: false,
       text,
-      id: new Date().getTime()
-    }
-    let arr = [...this.state.arr, obj]
-    let showArr = this.state.status === 'all' || this.state.status === 'unfinished' ? [...this.state.showArr, obj] : this.state.showArr
+      id: new Date().getTime(),
+    };
+    let arr = [...this.state.arr, obj];
+    let showArr =
+      this.state.status === "all" || this.state.status === "unfinished"
+        ? [...this.state.showArr, obj]
+        : this.state.showArr;
     this.setState({
       arr,
-      text: '',
-      showArr
-    })
+      text: "",
+      showArr,
+    });
   }
-  delList (id) {
-    let arr = this.state.arr, index = this.getArrIndex(id)
-    arr[index].del = true
+  delList(id) {
+    let arr = this.state.arr,
+      index = this.getArrIndex(id);
+    arr[index].del = true;
     this.setState(
       {
-        arr
-      }, () => {
-        this.switchShowArr(this.state.status)
+        arr,
+      },
+      () => {
+        this.switchShowArr(this.state.status);
       }
-    )
+    );
   }
   recoverItem(id) {
-    let arr = this.state.arr, index = this.getArrIndex(id)
-    arr[index].del = false
+    let arr = this.state.arr,
+      index = this.getArrIndex(id);
+    arr[index].del = false;
     this.setState(
       {
-        arr
-      }, () => {
-        this.switchShowArr(this.state.status)
+        arr,
+      },
+      () => {
+        this.switchShowArr(this.state.status);
       }
-    )
+    );
   }
   checkBox(id) {
-    let arr = this.state.arr, index = this.getArrIndex(id)
-    arr[index].checked = !arr[index].checked
+    let arr = this.state.arr,
+      index = this.getArrIndex(id);
+    arr[index].checked = !arr[index].checked;
     this.setState({
-      arr
-    })
+      arr,
+    });
   }
   getArrIndex(id) {
-      let index = -1
-      this.state.arr.forEach((item, v) => {
-        if (item.id === id) {
-          index = v
-        }
-      })
-      return index
+    let index = -1;
+    // foreach fixme
+    this.state.arr.forEach((item, v) => {
+      if (item.id === id) {
+        index = v;
+      }
+    });
+    return index;
   }
   switchShowArr(type) {
-    let showArr = []
-    if (type === 'all') {
-      showArr = this.state.arr.filter((item) => !item.del)
-    } else if (type === 'finished') {
-      showArr = this.state.arr.filter((item) => item.checked && !item.del)
-    } else if (type === 'unfinished') {
-      showArr = this.state.arr.filter((item) => !item.checked && !item.del)
-    } else if (type === 'del') {
-      showArr = this.state.arr.filter((item) => item.del)
+    let showArr = [];
+    if (type === "all") {
+      showArr = this.state.arr.filter((item) => !item.del);
+    } else if (type === "finished") {
+      showArr = this.state.arr.filter((item) => item.checked && !item.del);
+    } else if (type === "unfinished") {
+      showArr = this.state.arr.filter((item) => !item.checked && !item.del);
+    } else if (type === "del") {
+      showArr = this.state.arr.filter((item) => item.del);
     }
     this.setState({
       status: type,
-      showArr
-    })
+      showArr,
+    });
   }
   editItemText(n, v) {
-    let arr = this.state.arr, index = this.getArrIndex(n)
-    arr[index].text = v || arr[index].text
-    console.log(v)
+    let arr = this.state.arr,
+      index = this.getArrIndex(n);
+    arr[index].text = v || arr[index].text;
+    console.log(v);
     this.setState({
-      arr
-    })
+      arr,
+    });
   }
   render() {
-    console.log("父组件的render")
+    console.log("父组件的render");
     return (
       <div className="app">
         <p>使用redux</p>
-        <Count></Count>
-        <List></List>
+        <Count />
+        <List />
         <p>没用redux</p>
-        <input style={{ width: '190px'}}value={this.state.text} onChange={(e) => { this.inputHandle(e) }} onKeyDown={(e) => { this.keyDownHandle(e) }} placeholder="请输入任务" onBlur={(e) => { this.addList(e) }} />
+        <input
+          style={{ width: "190px" }}
+          value={this.state.text}
+          onChange={(e) => {
+            this.inputHandle(e);
+          }}
+          onKeyDown={(e) => {
+            this.keyDownHandle(e);
+          }}
+          placeholder="请输入任务"
+          onBlur={(e) => {
+            this.addList(e);
+          }}
+        />
         <div>
-          {this.state.showArr.length > 0 ? this.state.showArr.map((item, index) => <ListItem key={item.id} 
-          index={item.id} pdata={item} 
-          delList={(n) => () => { this.delList(n) }} 
-          recoverItem={(n) => () => { this.recoverItem(n)}} 
-          editItemText={(n,v)  => { this.editItemText(n, v) }}
-          checkBox={(n) => () => { this.checkBox(n) }}></ListItem>)
-        : (<span>暂无数据...</span>)}
+          {this.state.showArr.length > 0 ? (
+            this.state.showArr.map((item, index) => (
+              <ListItem
+                key={item.id}
+                index={item.id}
+                pdata={item}
+                delList={(n) => () => {
+                  this.delList(n);
+                }}
+                recoverItem={(n) => () => {
+                  this.recoverItem(n);
+                }}
+                editItemText={(n, v) => {
+                  this.editItemText(n, v);
+                }}
+                checkBox={(n) => () => {
+                  this.checkBox(n);
+                }}
+              ></ListItem>
+            ))
+          ) : (
+            <span>暂无数据...</span>
+          )}
         </div>
-        <button onClick={() => { this.switchShowArr('all')}}>all</button>
-        <button onClick={() => { this.switchShowArr('finished') }}>finished</button>
-        <button onClick={() => { this.switchShowArr('unfinished') }}>unfinished</button>
-        <button onClick={() => { this.switchShowArr('del') }}>del</button>
+        <button
+          onClick={() => {
+            this.switchShowArr("all");
+          }}
+        >
+          all
+        </button>
+        <button
+          onClick={() => {
+            this.switchShowArr("finished");
+          }}
+        >
+          finished
+        </button>
+        <button
+          onClick={() => {
+            this.switchShowArr("unfinished");
+          }}
+        >
+          unfinished
+        </button>
+        <button
+          onClick={() => {
+            this.switchShowArr("del");
+          }}
+        >
+          del
+        </button>
       </div>
     );
   }
--- src/page/ListItem.js
+++ src/page/ListItem.js
@@ -1,64 +1,95 @@
-import React, { Component } from 'react'
-import '../static/css/listItem.css'
+import React, { Component } from "react";
+import "../static/css/listItem.css";
 
 class ListItem extends Component {
-    constructor(props) {
-        super(props);
-        console.log('ListItem组件的初始化', props.pdata.text)
-        this.state= {
-            edit: false,
-            text: props.pdata.text
-        }
-        this.inputRef = React.createRef();
+  constructor(props) {
+    super(props);
+    console.log("ListItem组件的初始化", props.pdata.text);
+    this.state = {
+      edit: false,
+      text: props.pdata.text,
+    };
+    this.inputRef = React.createRef();
+  }
+  editItem(checked) {
+    if (checked) {
+      alert("已经完成的不能修改啊");
+      return;
     }
-    editItem(checked) {
-        if (checked) {
-            alert("已经完成的不能修改啊")
-            return
-        }
-        this.setState({
-            edit: true
-        }, () => {
-            this.inputRef.current.focus()
-        })
+    this.setState(
+      {
+        edit: true,
+      },
+      () => {
+        this.inputRef.current.focus();
+      }
+    );
+  }
+  editItemText(e) {
+    this.props.editItemText(this.props.index, this.state.text);
+    this.setState({
+      edit: false,
+    });
+  }
+  handleKeyEnter(e) {
+    if (e.keyCode === 13) {
+      this.editItemText(e);
     }
-    editItemText(e) {
-        this.props.editItemText(this.props.index, this.state.text)
-        this.setState({
-            edit: false
-        })
+  }
+  handleInputChange(e) {
+    this.setState({
+      text: e.target.value,
+    });
+  }
+  componentDidUpdate(oldProps) {
+    if (oldProps.pdata.text !== this.props.pdata.text) {
+      this.setState({
+        text: this.props.pdata.text,
+      });
     }
-    handleKeyEnter(e) {
-        if (e.keyCode === 13) {
-            this.editItemText(e)
-        }
-    }
-    handleInputChange(e) {
-        this.setState({
-            text: e.target.value
-        })
-    }
-    componentDidUpdate(oldProps) {
-        if (oldProps.pdata.text !== this.props.pdata.text) {
-            this.setState({
-                text: this.props.pdata.text
-            });
-        }
-    }
-    render() {
-        const { pdata, checkBox, delList, index, recoverItem} = this.props
-        return (<div>
-            <input type="checkbox" onChange={checkBox(index)} defaultChecked={pdata.checked}></input>
-            <span style={{ display: !this.state.edit ? 'inline-block' : 'none' }} onClick={() => { this.editItem(pdata.checked)}}  className={pdata.checked ? 'text finish' : 'text'}>{pdata.text}</span>
-            <input 
-            onBlur={(e) => {this.editItemText(e)}} 
-            onKeyDown={(e) => { this.handleKeyEnter(e)}} 
-            ref={this.inputRef} 
-            onChange={(e) => { this.handleInputChange(e) }}
-            style={{display: this.state.edit? 'inline-block' : 'none'}} 
-            type="text" value={this.state.text}></input>
-            {pdata.del ? (<button onClick={recoverItem(index)}>recover</button>): (<button onClick = { delList(index) }>del</button>)}
-        </div>);
-    }
+  }
+  render() {
+    const { pdata, checkBox, delList, index, recoverItem } = this.props;
+    return (
+      <div>
+        <input
+          type="checkbox"
+          onChange={checkBox(index)}
+          defaultChecked={pdata.checked}
+        ></input>
+        <span
+          style={{ display: !this.state.edit ? "inline-block" : "none" }}
+          onClick={() => {
+            this.editItem(pdata.checked);
+          }}
+          className={pdata.checked ? "text finish" : "text"}
+        >
+          {pdata.text}
+        </span>
+        <input
+          onBlur={(e) => {
+            this.editItemText(e);
+          }}
+          onKeyDown={(e) => {
+            this.handleKeyEnter(e);
+          }}
+          ref={this.inputRef}
+          onChange={(e) => {
+            this.handleInputChange(e);
+          }}
+          style={{ display: this.state.edit ? "inline-block" : "none" }}
+          type="text"
+          value={this.state.text}
+        ></input>
+        {pdata.del ? (
+            // fixme
+          <button onClick={recoverItem(index)}>recover</button>
+        ) : (
+            // fixme 
+          <button onClick={delList(index)}>del</button>
+        )}
+      </div>
+    );
+  }
 }
 export default ListItem;
