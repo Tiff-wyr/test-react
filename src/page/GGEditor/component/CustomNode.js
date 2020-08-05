06704ac89b8a5a9c0a9691bce71d3d2fe3beba6a
gengfeiyang <feiyanggeng@yufuid.com>
2020-08-05 19:19:08 +0800

    hh

--- src/page/GGEditor/component/CustomNode.js
+++ src/page/GGEditor/component/CustomNode.js
@@ -1,40 +1,126 @@
 import React from "react";
-import { RegisterNode } from "gg-editor";
+import GGEditor, { Flow, RegisterNode, setAnchorPointsState } from "gg-editor";
+import styles from "./index.less";
 
-class CustomNode extends React.Component {
-  render() {
-    const config = {
-      draw(item) {
-        // 绘制图标
-        const group = item.getGraphicGroup();
-        const model = item.getModel();
+const data = {
+  nodes: [
+    {
+      id: "0",
+      label: "Node",
+      x: 50,
+      y: 50,
+    },
+    {
+      id: "1",
+      label: "Node",
+      x: 50,
+      y: 200,
+    },
+  ],
+  edges: [
+    {
+      label: "Label",
+      source: "0",
+      sourceAnchor: 1,
+      target: "1",
+      targetAnchor: 0,
+    },
+  ],
+};
 
-        const keyShape = group.addShape("image", {
-          attrs: {
-            x: -15,
-            y: -25,
-            width: 30,
-            height: 30,
-            img: model.icon,
+function App() {
+  return (
+    <GGEditor>
+      <Flow
+        className={styles.graph}
+        data={data}
+        graphConfig={{ defaultNode: { type: "customNode" } }}
+        // graphConfig={{ defaultNode: { type: 'customStartNode' } }}
+        // graphConfig={{ defaultNode: { type: 'customInternalNode', size: 50 } }}
+      />
+      <RegisterNode
+        name="customNode"
+        config={{
+          getCustomConfig(model) {
+            return {
+              wrapperStyle: {
+                fill: "#000000",
+              },
+            };
           },
-        });
+        }}
+        extend="bizFlowNode"
+      />
+      <RegisterNode
+        name="customStartNode"
+        config={{
+          getCustomConfig(model) {
+            return {
+              size: [80, 40],
+              wrapperStyle: {
+                fill: "#5E6A7D",
+              },
+              contentStyle: {
+                fill: "#5E6A7D",
+              },
+              labelStyle: {
+                fill: "#FFFFFF",
+              },
+            };
+          },
+          getAnchorPoints() {
+            return [
+              [0.5, 0],
+              [0.5, 1],
+              [0, 0.5],
+              [1, 0.5],
+            ];
+          },
+        }}
+        extend="bizFlowNode"
+      />
+      <RegisterNode
+        name="customInternalNode"
+        config={{
+          setState(name, value, item) {
+            setAnchorPointsState.call(
+              this,
+              name,
+              value,
+              item,
+              (item, anchorPoint) => {
+                const { width, height } = item.getKeyShape().getBBox();
 
-        // 绘制标签
-        // GGEditor.drawLabel(item);
+                const [x, y] = anchorPoint;
 
-        return keyShape;
-      },
+                return {
+                  x: width * x - width / 2,
+                  y: height * y - height / 2,
+                };
+              },
+              (item, anchorPoint) => {
+                const { width, height } = item.getKeyShape().getBBox();
 
-      anchor: [
-        [0.5, 0], // 上边中点
-        [0.5, 1], // 底边中点
-      ],
-    };
+                const [x, y] = anchorPoint;
 
-    return (
-      <RegisterNode name="custom-node" config={config} extend={"flow-rect"} />
-    );
-  }
+                return {
+                  x: width * x - width / 2,
+                  y: height * y - height / 2,
+                };
+              }
+            );
+          },
+          getAnchorPoints() {
+            return [
+              [0.5, 0],
+              [0.5, 1],
+              [0, 0.5],
+              [1, 0.5],
+            ];
+          },
+        }}
+        extend="circle"
+      />
+    </GGEditor>
+  );
 }
-
-export default CustomNode;
--- src/page/GGEditor/index.js
+++ src/page/GGEditor/index.js
@@ -1,65 +1,132 @@
 import React from "react";
-import GGEditor, { Flow, EditableLabel } from "gg-editor";
+import GGEditor, { Flow, RegisterNode, setAnchorPointsState } from "gg-editor";
+import "./index.css";
 
-import "./styles.css";
-import CustomNode from "./component/CustomNode";
+const data = {
+  nodes: [
+    {
+      id: "0",
+      label: "Node",
+      x: 50,
+      y: 50,
+    },
+    {
+      id: "1",
+      label: "Node",
+      x: 50,
+      y: 200,
+    },
+  ],
+  edges: [
+    {
+      label: "Label",
+      source: "0",
+      sourceAnchor: 1,
+      target: "1",
+      targetAnchor: 0,
+    },
+  ],
+};
 
-export default function GGEditorWrap() {
-  const data = {
-    nodes: [
-      {
-        type: "node",
-        size: "100*100",
-        shape: "custom-node",
-        color: "#FA8C16",
-        label: "Ant Design",
-        labelOffsetY: 20,
-        icon: "//img.alicdn.com/tfs/TB1gXH2ywHqK1RjSZFPXXcwapXa-200-200.svg",
-        x: 100,
-        y: 100,
-        id: "ea1184e8",
-        index: 0,
-      },
-      {
-        type: "node",
-        size: "100*100",
-        shape: "custom-node",
-        color: "#FA8C16",
-        label: "React",
-        labelOffsetY: 20,
-        icon: "//img.alicdn.com/tfs/TB1OzAmyyLaK1RjSZFxXXamPFXa-200-200.svg",
-        x: 100,
-        y: 300,
-        id: "481fbb1a",
-        index: 2,
-      },
-    ],
-    edges: [
-      {
-        source: "ea1184e8",
-        sourceAnchor: 1,
-        target: "481fbb1a",
-        targetAnchor: 0,
-        id: "7989ac70",
-        index: 1,
-      },
-    ],
-  };
+function GGEditorWrap() {
   return (
-    <GGEditor className="editor">
+    <GGEditor
+      style={{
+        height: "800px",
+      }}
+    >
       <Flow
-        autoZoom={true}
-        zoomOut={false}
-        zoomIn={false}
-        className="editorBd"
+        className={"graph"}
         data={data}
-        fixView={false}
-        graph={{
-          edgeDefaultShape: "flow-polyline-round",
-        }}
+        graphConfig={{ defaultNode: { type: "customNode" } }}
+        // graphConfig={{ defaultNode: { type: 'customStartNode' } }}
+        // graphConfig={{ defaultNode: { type: 'customInternalNode', size: 50 } }}
       />
-      <CustomNode />
-      <EditableLabel />
+      <RegisterNode
+        name="customNode"
+        config={{
+          getCustomConfig(model) {
+            return {
+              wrapperStyle: {
+                fill: "#000000",
+              },
+            };
+          },
+        }}
+        extend="bizFlowNode"
+      />
+      <RegisterNode
+        name="customStartNode"
+        config={{
+          getCustomConfig(model) {
+            return {
+              size: [80, 40],
+              wrapperStyle: {
+                fill: "#5E6A7D",
+              },
+              contentStyle: {
+                fill: "#5E6A7D",
+              },
+              labelStyle: {
+                fill: "#FFFFFF",
+              },
+            };
+          },
+          getAnchorPoints() {
+            return [
+              [0.5, 0],
+              [0.5, 1],
+              [0, 0.5],
+              [1, 0.5],
+            ];
+          },
+        }}
+        extend="bizFlowNode"
+      />
+      <RegisterNode
+        name="customInternalNode"
+        config={{
+          setState(name, value, item) {
+            setAnchorPointsState.call(
+              this,
+              name,
+              value,
+              item,
+              (item, anchorPoint) => {
+                const { width, height } = item.getKeyShape().getBBox();
+
+                const [x, y] = anchorPoint;
+
+                return {
+                  x: width * x - width / 2,
+                  y: height * y - height / 2,
+                };
+              },
+              (item, anchorPoint) => {
+                const { width, height } = item.getKeyShape().getBBox();
+
+                const [x, y] = anchorPoint;
+
+                return {
+                  x: width * x - width / 2,
+                  y: height * y - height / 2,
+                };
+              }
+            );
+          },
+          getAnchorPoints() {
+            return [
+              [0.5, 0],
+              [0.5, 1],
+              [0, 0.5],
+              [1, 0.5],
+            ];
+          },
+        }}
+        extend="circle"
+      />
     </GGEditor>
   );
 }
+
+export default GGEditorWrap;
